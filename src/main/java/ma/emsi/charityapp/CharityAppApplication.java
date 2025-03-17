0112691e0a80a186ca916073c1d2657e20a16251
ismailstarallah <ismail.starallah@emsi-edu.ma>
2025-03-17 10:30:46 +0000

    Initial commit

---
+++ src/main/java/ma/emsi/charityapp/CharityAppApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.charityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/emsi/charityapp/CharityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.charityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
