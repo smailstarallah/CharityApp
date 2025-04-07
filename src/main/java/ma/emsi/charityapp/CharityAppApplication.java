b12e01a7bc35ab50da1375e91a4f596ecb1b2f13
ismailstarallah <ismail.starallah@emsi-edu.ma>
2025-04-07 10:27:36 +0100

    Add validation and JPA dependencies, update sample data in CharityAppApplication, and create ValidationConfig class

--- src/main/java/ma/emsi/charityapp/CharityAppApplication.java
+++ src/main/java/ma/emsi/charityapp/CharityAppApplication.java
@@ -5,11 +5,11 @@ import ma.emsi.charityapp.entities.RegularUser;
 import ma.emsi.charityapp.entities.SuperAdmin;
 import ma.emsi.charityapp.repositories.RegularUserRepository;
 import ma.emsi.charityapp.repositories.SuperAdminRepository;
-import ma.emsi.charityapp.services.RegularUserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CharityAppApplication implements CommandLineRunner {
@@ -22,12 +22,12 @@ public class CharityAppApplication implements CommandLineRunner {
         SpringApplication.run(CharityAppApplication.class, args);
     }
 
-    public void run(String... args) throws Exception {
-        regularUserRepository.save(new RegularUser("test", "test", "tesat@gmail.ma", 123456789, new java.util.Date(), "test", UserType.REGULAR_USER));
-        regularUserRepository.save(new RegularUser("test", "test", "taest@gmail.ma", 12346789, new java.util.Date(), "test", UserType.REGULAR_USER));
-        regularUserRepository.save(new RegularUser("test", "test", "testa@gmail.ma", 1234789, new java.util.Date(), "test", UserType.REGULAR_USER));
-        regularUserRepository.save(new RegularUser("test", "test", "ateastatest@gmail.ma", 12456789, new java.util.Date(), "test", UserType.REGULAR_USER));
-        superAdminRepository.save(new SuperAdmin("test", "test", "ateastatest@gmail.ma", 12456789, new java.util.Date(), "test"));
-        superAdminRepository.save(new SuperAdmin("test", "test", "ateast@gmail.ma", 1245789, new java.util.Date(), "test"));
+    public void run(String... args) {
+            regularUserRepository.save(new RegularUser("test", "test", "tesat@gmail.ma", "0634567891", new java.util.Date(), "test", UserType.REGULAR_USER));
+            regularUserRepository.save(new RegularUser("test", "test", "yui@gmail.ma", "0600000098", new java.util.Date(), "test", UserType.REGULAR_USER));
+            regularUserRepository.save(new RegularUser("test", "test", "taest@gmail.ma", "0623478987", new java.util.Date(), "test", UserType.REGULAR_USER));
+            regularUserRepository.save(new RegularUser("test", "test", "ateastatest@gmail.ma", "0612456789", new java.util.Date(), "test", UserType.REGULAR_USER));
+            superAdminRepository.save(new SuperAdmin("test", "test", "azer@gmail.ma", "0624567891", new java.util.Date(), "test"));
+            superAdminRepository.save(new SuperAdmin("test", "test", "aze@gmail.ma", "0624578912", new java.util.Date(), "test"));
     }
 }
